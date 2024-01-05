ef098c1e2b52ed35e55bf8f83e4914b957d5fae0
Lasse Öörni <loorni@gmail.com>
2024-01-05 23:03:43 +0200

    Update copyright year.

--- Assets/Scripts/SID.cs
+++ Assets/Scripts/SID.cs
@@ -1,6 +1,6 @@
 ﻿// MIT License
 //
-// Copyright (c) 2018-2020 Lasse Oorni
+// Copyright (c) 2018-2024 Lasse Oorni
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
